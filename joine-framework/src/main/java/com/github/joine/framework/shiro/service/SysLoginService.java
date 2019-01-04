413de85751939708c93ce7494a0d21ae66a81be9
jenphyjohn <117083959@qq.com>
2019-01-04 12:48:32 +0800

    优化代码

--- joine-framework/src/main/java/com/github/joine/framework/shiro/service/SysLoginService.java
+++ joine-framework/src/main/java/com/github/joine/framework/shiro/service/SysLoginService.java
@@ -1,10 +1,5 @@
 package com.github.joine.framework.shiro.service;
 
-import com.github.joine.framework.manager.AsyncManager;
-import com.github.joine.framework.manager.factory.AsyncFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 import com.github.joine.common.constant.Constants;
 import com.github.joine.common.constant.ShiroConstants;
 import com.github.joine.common.constant.UserConstants;
@@ -15,22 +10,20 @@ import com.github.joine.framework.manager.factory.AsyncFactory;
 import com.github.joine.framework.util.MessageUtils;
 import com.github.joine.framework.util.ServletUtils;
 import com.github.joine.framework.util.ShiroUtils;
-import com.github.joine.framework.web.exception.user.CaptchaException;
-import com.github.joine.framework.web.exception.user.UserBlockedException;
-import com.github.joine.framework.web.exception.user.UserDeleteException;
-import com.github.joine.framework.web.exception.user.UserNotExistsException;
-import com.github.joine.framework.web.exception.user.UserPasswordNotMatchException;
+import com.github.joine.framework.web.exception.user.*;
 import com.github.joine.system.domain.SysUser;
 import com.github.joine.system.service.ISysUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 /**
  * 登录校验方法
- * 
+ *
  * @author JenphyJohn
  */
 @Component
-public class SysLoginService
-{
+public class SysLoginService {
     @Autowired
     private SysPasswordService passwordService;
 
@@ -40,32 +33,27 @@ public class SysLoginService
     /**
      * 登录
      */
-    public SysUser login(String username, String password)
-    {
+    public SysUser login(String username, String password) {
         // 验证码校验
-        if (!StringUtils.isEmpty(ServletUtils.getRequest().getAttribute(ShiroConstants.CURRENT_CAPTCHA)))
-        {
+        if (!StringUtils.isEmpty(ServletUtils.getRequest().getAttribute(ShiroConstants.CURRENT_CAPTCHA))) {
             AsyncManager.me().execute(AsyncFactory.recordLogininfor(username, Constants.LOGIN_FAIL, MessageUtils.message("user.jcaptcha.error")));
             throw new CaptchaException();
         }
         // 用户名或密码为空 错误
-        if (StringUtils.isEmpty(username) || StringUtils.isEmpty(password))
-        {
+        if (StringUtils.isEmpty(username) || StringUtils.isEmpty(password)) {
             AsyncManager.me().execute(AsyncFactory.recordLogininfor(username, Constants.LOGIN_FAIL, MessageUtils.message("not.null")));
             throw new UserNotExistsException();
         }
         // 密码如果不在指定范围内 错误
         if (password.length() < UserConstants.PASSWORD_MIN_LENGTH
-                || password.length() > UserConstants.PASSWORD_MAX_LENGTH)
-        {
+                || password.length() > UserConstants.PASSWORD_MAX_LENGTH) {
             AsyncManager.me().execute(AsyncFactory.recordLogininfor(username, Constants.LOGIN_FAIL, MessageUtils.message("user.password.not.match")));
             throw new UserPasswordNotMatchException();
         }
 
         // 用户名不在指定范围内 错误
         if (username.length() < UserConstants.USERNAME_MIN_LENGTH
-                || username.length() > UserConstants.USERNAME_MAX_LENGTH)
-        {
+                || username.length() > UserConstants.USERNAME_MAX_LENGTH) {
             AsyncManager.me().execute(AsyncFactory.recordLogininfor(username, Constants.LOGIN_FAIL, MessageUtils.message("user.password.not.match")));
             throw new UserPasswordNotMatchException();
         }
@@ -73,30 +61,25 @@ public class SysLoginService
         // 查询用户信息
         SysUser user = userService.selectUserByLoginName(username);
 
-        if (user == null && maybeMobilePhoneNumber(username))
-        {
+        if (user == null && maybeMobilePhoneNumber(username)) {
             user = userService.selectUserByPhoneNumber(username);
         }
 
-        if (user == null && maybeEmail(username))
-        {
+        if (user == null && maybeEmail(username)) {
             user = userService.selectUserByEmail(username);
         }
 
-        if (user == null)
-        {
+        if (user == null) {
             AsyncManager.me().execute(AsyncFactory.recordLogininfor(username, Constants.LOGIN_FAIL, MessageUtils.message("user.not.exists")));
             throw new UserNotExistsException();
         }
-        
-        if (UserStatus.DELETED.getCode().equals(user.getDelFlag()))
-        {
+
+        if (UserStatus.DELETED.getCode().equals(user.getDelFlag())) {
             AsyncManager.me().execute(AsyncFactory.recordLogininfor(username, Constants.LOGIN_FAIL, MessageUtils.message("user.password.delete")));
             throw new UserDeleteException();
         }
-        
-        if (UserStatus.DISABLE.getCode().equals(user.getStatus()))
-        {
+
+        if (UserStatus.DISABLE.getCode().equals(user.getStatus())) {
             AsyncManager.me().execute(AsyncFactory.recordLogininfor(username, Constants.LOGIN_FAIL, MessageUtils.message("user.blocked", user.getRemark())));
             throw new UserBlockedException(user.getRemark());
         }
@@ -108,19 +91,15 @@ public class SysLoginService
         return user;
     }
 
-    private boolean maybeEmail(String username)
-    {
-        if (!username.matches(UserConstants.EMAIL_PATTERN))
-        {
+    private boolean maybeEmail(String username) {
+        if (!username.matches(UserConstants.EMAIL_PATTERN)) {
             return false;
         }
         return true;
     }
 
-    private boolean maybeMobilePhoneNumber(String username)
-    {
-        if (!username.matches(UserConstants.MOBILE_PHONE_NUMBER_PATTERN))
-        {
+    private boolean maybeMobilePhoneNumber(String username) {
+        if (!username.matches(UserConstants.MOBILE_PHONE_NUMBER_PATTERN)) {
             return false;
         }
         return true;
@@ -129,8 +108,7 @@ public class SysLoginService
     /**
      * 记录登录信息
      */
-    public void recordLoginInfo(SysUser user)
-    {
+    public void recordLoginInfo(SysUser user) {
         user.setLoginIp(ShiroUtils.getIp());
         user.setLoginDate(DateUtils.getNowDate());
         userService.updateUserInfo(user);
