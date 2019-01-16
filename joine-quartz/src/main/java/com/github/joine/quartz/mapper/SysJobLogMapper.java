ae101ee2bcbb4e14fe568b3630818b5406976521
jenphyjohn <117083959@qq.com>
2019-01-16 13:20:00 +0800

    初始调度模块

---
+++ joine-quartz/src/main/java/com/github/joine/quartz/mapper/SysJobLogMapper.java
@@ -0,0 +1,59 @@
+package com.github.joine.quartz.mapper;
+
+import java.util.List;
+
+import com.github.joine.quartz.domain.SysJobLog;
+import com.github.joine.quartz.domain.SysJobLog;
+
+/**
+ * 调度任务日志信息 数据层
+ * 
+ * @author JenphyJohn
+ */
+public interface SysJobLogMapper
+{
+    /**
+     * 获取quartz调度器日志的计划任务
+     * 
+     * @param jobLog 调度日志信息
+     * @return 调度任务日志集合
+     */
+    List<SysJobLog> selectJobLogList(SysJobLog jobLog);
+
+    /**
+     * 通过调度任务日志ID查询调度信息
+     * 
+     * @param jobLogId 调度任务日志ID
+     * @return 调度任务日志对象信息
+     */
+    SysJobLog selectJobLogById(Long jobLogId);
+
+    /**
+     * 新增任务日志
+     * 
+     * @param jobLog 调度日志信息
+     * @return 结果
+     */
+    int insertJobLog(SysJobLog jobLog);
+
+    /**
+     * 批量删除调度日志信息
+     * 
+     * @param ids 需要删除的数据ID
+     * @return 结果
+     */
+    int deleteJobLogByIds(String[] ids);
+
+    /**
+     * 删除任务日志
+     * 
+     * @param jobId 调度日志ID
+     * @return 结果
+     */
+    int deleteJobLogById(Long jobId);
+
+    /**
+     * 清空任务日志
+     */
+    void cleanJobLog();
+}
