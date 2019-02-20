802da7471a8f5c677f820af1aa256a678db21a97
jenphyjohn <117083959@qq.com>
2019-02-20 11:10:27 +0800

    调整格式

--- joine-common/src/main/java/com/github/joine/common/constant/Constants.java
+++ joine-common/src/main/java/com/github/joine/common/constant/Constants.java
@@ -2,11 +2,10 @@ package com.github.joine.common.constant;
 
 /**
  * 通用常量信息
- * 
+ *
  * @author JenphyJohn
  */
-public class Constants
-{
+public class Constants {
     /**
      * UTF-8 字符集
      */
