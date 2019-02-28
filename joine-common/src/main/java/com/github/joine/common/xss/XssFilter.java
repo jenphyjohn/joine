d71b7720adb6883d8f00de21307d2c72da9077e2
jenphyjohn <117083959@qq.com>
2019-02-28 10:02:47 +0800

    调整格式

--- joine-common/src/main/java/com/github/joine/common/xss/XssFilter.java
+++ joine-common/src/main/java/com/github/joine/common/xss/XssFilter.java
@@ -13,11 +13,10 @@ import java.util.regex.Pattern;
 
 /**
  * 防止XSS攻击的过滤器
- * 
+ *
  * @author JenphyJohn
  */
-public class XssFilter implements Filter
-{
+public class XssFilter implements Filter {
     /**
      * 排除链接
      */
@@ -29,32 +28,26 @@ public class XssFilter implements Filter
     public boolean enabled = false;
 
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException
-    {
+    public void init(FilterConfig filterConfig) throws ServletException {
         String tempExcludes = filterConfig.getInitParameter("excludes");
         String tempEnabled = filterConfig.getInitParameter("enabled");
-        if (StringUtils.isNotEmpty(tempExcludes))
-        {
+        if (StringUtils.isNotEmpty(tempExcludes)) {
             String[] url = tempExcludes.split(",");
-            for (int i = 0; url != null && i < url.length; i++)
-            {
+            for (int i = 0; url != null && i < url.length; i++) {
                 excludes.add(url[i]);
             }
         }
-        if (StringUtils.isNotEmpty(tempEnabled))
-        {
+        if (StringUtils.isNotEmpty(tempEnabled)) {
             enabled = Boolean.valueOf(tempEnabled);
         }
     }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException
-    {
+            throws IOException, ServletException {
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse resp = (HttpServletResponse) response;
-        if (handleExcludeURL(req, resp))
-        {
+        if (handleExcludeURL(req, resp)) {
             chain.doFilter(request, response);
             return;
         }
@@ -62,23 +55,18 @@ public class XssFilter implements Filter
         chain.doFilter(xssRequest, response);
     }
 
-    private boolean handleExcludeURL(HttpServletRequest request, HttpServletResponse response)
-    {
-        if (!enabled)
-        {
+    private boolean handleExcludeURL(HttpServletRequest request, HttpServletResponse response) {
+        if (!enabled) {
             return true;
         }
-        if (excludes == null || excludes.isEmpty())
-        {
+        if (excludes == null || excludes.isEmpty()) {
             return false;
         }
         String url = request.getServletPath();
-        for (String pattern : excludes)
-        {
+        for (String pattern : excludes) {
             Pattern p = Pattern.compile("^" + pattern);
             Matcher m = p.matcher(url);
-            if (m.find())
-            {
+            if (m.find()) {
                 return true;
             }
         }
@@ -86,8 +74,7 @@ public class XssFilter implements Filter
     }
 
     @Override
-    public void destroy()
-    {
+    public void destroy() {
 
     }
 }
