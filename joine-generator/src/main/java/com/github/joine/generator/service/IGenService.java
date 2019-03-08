1ebb48fa7c586998503c2929817f2c10d5aa149e
jenphyjohn <117083959@qq.com>
2019-03-08 08:45:59 +0800

    生成代码模块统一

--- joine-generator/src/main/java/com/github/joine/generator/service/IGenService.java
+++ joine-generator/src/main/java/com/github/joine/generator/service/IGenService.java
@@ -1,38 +1,36 @@
 package com.github.joine.generator.service;
 
-import java.util.List;
+import com.github.joine.generator.domain.TableInfo;
 
-import com.github.joine.generator.domain.TableInfo;
-import com.github.joine.generator.domain.TableInfo;
+import java.util.List;
 
 /**
  * 代码生成 服务层
- * 
+ *
  * @author JenphyJohn
  */
-public interface IGenService
-{
+public interface IGenService {
     /**
-     * 查询ry数据库表信息
-     * 
+     * 查询je数据库表信息
+     *
      * @param tableInfo 表信息
      * @return 数据库表列表
      */
-    public List<TableInfo> selectTableList(TableInfo tableInfo);
+    List<TableInfo> selectTableList(TableInfo tableInfo);
 
     /**
      * 生成代码
-     * 
+     *
      * @param tableName 表名称
      * @return 数据
      */
-    public byte[] generatorCode(String tableName);
+    byte[] generatorCode(String tableName);
 
     /**
      * 批量生成代码
-     * 
+     *
      * @param tableNames 表数组
      * @return 数据
      */
-    public byte[] generatorCode(String[] tableNames);
+    byte[] generatorCode(String[] tableNames);
 }
--- joine-generator/src/main/java/com/github/joine/generator/service/impl/GenServiceImpl.java
+++ joine-generator/src/main/java/com/github/joine/generator/service/impl/GenServiceImpl.java
@@ -1,12 +1,5 @@
 package com.github.joine.generator.service.impl;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.StringWriter;
-import java.util.List;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
-
 import com.github.joine.common.config.Global;
 import com.github.joine.common.constant.Constants;
 import com.github.joine.common.utils.StringUtils;
@@ -24,50 +17,45 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.github.joine.common.config.Global;
-import com.github.joine.common.constant.Constants;
-import com.github.joine.common.utils.StringUtils;
-import com.github.joine.generator.domain.ColumnInfo;
-import com.github.joine.generator.domain.TableInfo;
-import com.github.joine.generator.mapper.GenMapper;
-import com.github.joine.generator.service.IGenService;
-import com.github.joine.generator.util.GenUtils;
-import com.github.joine.generator.util.VelocityInitializer;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 /**
  * 代码生成 服务层处理
- * 
+ *
  * @author JenphyJohn
  */
 @Service
-public class GenServiceImpl implements IGenService
-{
+public class GenServiceImpl implements IGenService {
     private static final Logger log = LoggerFactory.getLogger(GenServiceImpl.class);
 
     @Autowired
     private GenMapper genMapper;
 
     /**
-     * 查询ry数据库表信息
-     * 
+     * 查询je数据库表信息
+     *
      * @param tableInfo 表信息
      * @return 数据库表列表
      */
     @Override
-    public List<TableInfo> selectTableList(TableInfo tableInfo)
-    {
+    public List<TableInfo> selectTableList(TableInfo tableInfo) {
         return genMapper.selectTableList(tableInfo);
     }
 
     /**
      * 生成代码
-     * 
+     *
      * @param tableName 表名称
      * @return 数据
      */
     @Override
-    public byte[] generatorCode(String tableName)
-    {
+    public byte[] generatorCode(String tableName) {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         ZipOutputStream zip = new ZipOutputStream(outputStream);
         // 查询表信息
@@ -82,17 +70,15 @@ public class GenServiceImpl implements IGenService
 
     /**
      * 批量生成代码
-     * 
+     *
      * @param tableNames 表数组
      * @return 数据
      */
     @Override
-    public byte[] generatorCode(String[] tableNames)
-    {
+    public byte[] generatorCode(String[] tableNames) {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         ZipOutputStream zip = new ZipOutputStream(outputStream);
-        for (String tableName : tableNames)
-        {
+        for (String tableName : tableNames) {
             // 查询表信息
             TableInfo table = genMapper.selectTableByName(tableName);
             // 查询列信息
@@ -107,8 +93,7 @@ public class GenServiceImpl implements IGenService
     /**
      * 生成代码
      */
-    public void generatorCode(TableInfo table, List<ColumnInfo> columns, ZipOutputStream zip)
-    {
+    public void generatorCode(TableInfo table, List<ColumnInfo> columns, ZipOutputStream zip) {
         // 表名转换成Java属性名
         String className = GenUtils.tableToJava(table.getTableName());
         table.setClassName(className);
@@ -127,22 +112,18 @@ public class GenServiceImpl implements IGenService
 
         // 获取模板列表
         List<String> templates = GenUtils.getTemplates();
-        for (String template : templates)
-        {
+        for (String template : templates) {
             // 渲染模板
             StringWriter sw = new StringWriter();
             Template tpl = Velocity.getTemplate(template, Constants.UTF8);
             tpl.merge(context, sw);
-            try
-            {
+            try {
                 // 添加到zip
                 zip.putNextEntry(new ZipEntry(GenUtils.getFileName(template, table, moduleName)));
                 IOUtils.write(sw.toString(), zip, Constants.UTF8);
                 IOUtils.closeQuietly(sw);
                 zip.closeEntry();
-            }
-            catch (IOException e)
-            {
+            } catch (IOException e) {
                 log.error("渲染模板失败，表名：" + table.getTableName(), e);
             }
         }
