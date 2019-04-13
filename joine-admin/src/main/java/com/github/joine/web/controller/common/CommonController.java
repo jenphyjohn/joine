efa87c5e93ccc44b66825fed119a6dc651027f6a
jenphyjohn <117083959@qq.com>
2019-04-13 16:51:09 +0800

    优化代码

--- joine-admin/src/main/java/com/github/joine/web/controller/common/CommonController.java
+++ joine-admin/src/main/java/com/github/joine/web/controller/common/CommonController.java
@@ -45,7 +45,7 @@ public class CommonController {
     public void fileDownload(String fileName, Boolean delete, HttpServletResponse response, HttpServletRequest request) {
         try {
             if (!FileUtils.isValidFilename(fileName)) {
-                throw new Exception(StringUtils.format(" 文件名称({})非法，不允许下载。 ", fileName));
+                throw new Exception(StringUtils.format("文件名称({})非法，不允许下载。 ", fileName));
             }
             String realFileName = System.currentTimeMillis() + fileName.substring(fileName.indexOf("_") + 1);
             String filePath = Global.getDownloadPath() + fileName;
