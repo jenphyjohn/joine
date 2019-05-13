631bdb02487389e6473f69eb2e1e7008f9b15260
jenphyjohn <117083959@qq.com>
2019-05-13 22:07:21 +0800

    test

--- joine-admin/src/main/java/com/github/joine/JoineServletInitializer.java
+++ joine-admin/src/main/java/com/github/joine/JoineServletInitializer.java
@@ -4,7 +4,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
- * web容器中进行部署
+ * Web容器中进行部署
  *
  * @author JenphyJohn
  */
