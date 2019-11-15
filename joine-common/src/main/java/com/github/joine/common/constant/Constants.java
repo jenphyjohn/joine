6da51036f5b6a2979ad21e32e80dfbe4a758363a
jenphyjohn <117083959@qq.com>
2019-11-15 09:31:34 +0800

    常量改为interface

--- joine-common/src/main/java/com/github/joine/common/constant/Constants.java
+++ joine-common/src/main/java/com/github/joine/common/constant/Constants.java
@@ -5,84 +5,84 @@ package com.github.joine.common.constant;
  *
  * @author JenphyJohn
  */
-public class Constants {
+public interface Constants {
     /**
      * UTF-8 字符集
      */
-    public static final String UTF8 = "UTF-8";
+    String UTF8 = "UTF-8";
 
     /**
      * 通用成功标识
      */
-    public static final String SUCCESS = "0";
+    String SUCCESS = "0";
 
     /**
      * 通用失败标识
      */
-    public static final String FAIL = "1";
+    String FAIL = "1";
 
     /**
      * 登录成功
      */
-    public static final String LOGIN_SUCCESS = "Success";
+    String LOGIN_SUCCESS = "Success";
 
     /**
      * 注销
      */
-    public static final String LOGOUT = "Logout";
+    String LOGOUT = "Logout";
 
     /**
      * 登录失败
      */
-    public static final String LOGIN_FAIL = "Error";
+    String LOGIN_FAIL = "Error";
 
     /**
      * 自动去除表前缀
      */
-    public static final String AUTO_REOMVE_PRE = "true";
+    String AUTO_REOMVE_PRE = "true";
 
     /**
      * 当前记录起始索引
      */
-    public static final String PAGE_NUM = "pageNum";
+    String PAGE_NUM = "pageNum";
 
     /**
      * 每页显示记录数
      */
-    public static final String PAGE_SIZE = "pageSize";
+    String PAGE_SIZE = "pageSize";
 
     /**
      * 排序列
      */
-    public static final String ORDER_BY_COLUMN = "orderByColumn";
+    String ORDER_BY_COLUMN = "orderByColumn";
 
     /**
      * 排序的方向 "desc" 或者 "asc".
      */
-    public static final String IS_ASC = "isAsc";
+    String IS_ASC = "isAsc";
 
     /**
      * 删除标志（0代表存在）.
      */
-    public static final String DEL_FLAG_FALSE = "0";
+    String DEL_FLAG_FALSE = "0";
 
     /**
      * 删除标志（2代表删除）.
      */
-    public static final String DEL_FLAG_TRUE = "2";
+    String DEL_FLAG_TRUE = "2";
 
     /**
      * 终端类型 APP
      */
-    public static final String APP_TYPE_MOBILE = "0";
+    String APP_TYPE_MOBILE = "0";
 
     /**
      * 终端类型 微信小程序
      */
-    public static final String APP_TYPE_MP = "1";
+    String APP_TYPE_MP = "1";
 
     /**
      * app类型 ios
      */
-    public static final int APP_TYPE_IOS = 1;
+    int APP_TYPE_IOS = 1;
 }
--- joine-common/src/main/java/com/github/joine/common/constant/PermissionConstants.java
+++ joine-common/src/main/java/com/github/joine/common/constant/PermissionConstants.java
@@ -5,34 +5,34 @@ package com.github.joine.common.constant;
  *
  * @author JenphyJohn
  */
-public class PermissionConstants {
+public interface PermissionConstants {
     /**
      * 新增权限
      */
-    public static final String ADD_PERMISSION = "add";
+    String ADD_PERMISSION = "add";
 
     /**
      * 修改权限
      */
-    public static final String EDIT_PERMISSION = "edit";
+    String EDIT_PERMISSION = "edit";
 
     /**
      * 删除权限
      */
-    public static final String REMOVE_PERMISSION = "remove";
+    String REMOVE_PERMISSION = "remove";
 
     /**
      * 导出权限
      */
-    public static final String EXPORT_PERMISSION = "export";
+    String EXPORT_PERMISSION = "export";
 
     /**
      * 显示权限
      */
-    public static final String VIEW_PERMISSION = "view";
+    String VIEW_PERMISSION = "view";
 
     /**
      * 查询权限
      */
-    public static final String LIST_PERMISSION = "list";
+    String LIST_PERMISSION = "list";
 }
